f1c7ac34fab45a1550daf68ba7661b1c5051dacd
tylim <limteikyee@gmail.com>
2019-08-15 01:43:09 +0800

    adding header component and links to home, shop page and contact

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,7 +3,7 @@ import Directory from '../../components/directory/directory.component.jsx'
 import './homepage.styles.scss'
 
 const HomePage = () => (
-	<div className='homePage'>
+	<div className='homepage'>
 		<Directory />
 	</div>
 )
